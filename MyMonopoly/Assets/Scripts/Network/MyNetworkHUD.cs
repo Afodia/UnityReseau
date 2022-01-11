ec4bb0500b96b074cf9ed1eaa8fc44cb449aebb0
Zargith <stanislas.hegron@gmail.com>
2022-01-11 11:57:58 +0100

    Fixed errors with HUD and my network manager

--- MyMonopoly/Assets/Scripts/Network/MyNetworkHUD.cs
+++ MyMonopoly/Assets/Scripts/Network/MyNetworkHUD.cs
@@ -4,64 +4,82 @@ using UnityEngine;
 using UnityEngine.UI;
 using Mirror;
 using TMPro;
+using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(NetworkManager))]
-public class MyNetworkHUD : NetworkBehaviour
+namespace Mirror
 {
-    NetworkManager manager;
-    [SerializeField] TMP_Text infosText;
-    [SerializeField] TMP_InputField ipAddress;
-    [SerializeField] Button connectGameButton;
-    [SerializeField] Button joinGameButton;
-
-    void Awake() {
-        manager = GetComponent<NetworkManager>();
-    }
-
-    public void StartHostingGame() {
-        manager.StartHost(); // starts both host and client
-    }
-
-    public void StopHostingGame() {
-        manager.StopHost(); // stop both host and client
-        infosText.text = "";
-    }
-
-    public void ConnectGame() {
-        manager.networkAddress = ipAddress.text;
-        connectGameButton.interactable = false;
-        infosText.text = $"Connecting to {manager.networkAddress}...";
-        manager.StartClient();
-    }
-
-    public void JoinGame() {
-        NetworkClient.Ready();
-        if (NetworkClient.localPlayer == null)
-            NetworkClient.AddPlayer();
-    }
-
-    public void LeaveGame() {
-        connectGameButton.interactable = true;
-        manager.StopClient();
-        if (joinGameButton.IsActive())
-            joinGameButton.enabled = false;
-        infosText.text = "";
-    }
-
-    void Update() {
-        UpdateStatusLabels();
-
-        if (NetworkClient.isConnected && !NetworkClient.ready && !joinGameButton.IsActive())
-            joinGameButton.enabled = true;
-    }
-
-    void UpdateStatusLabels()
+    [RequireComponent(typeof(NetworkManager))]
+    public class MyNetworkHUD : MonoBehaviour
     {
-        if (NetworkServer.active && NetworkClient.active) // host mode
-            infosText.text = $"<b>Host</b>: running via {Transport.activeTransport}";
-        else if (NetworkClient.isConnected) // client only
-            infosText.text = $"<b>Client</b>: connected to {manager.networkAddress} via {Transport.activeTransport}";
-        else if (!NetworkServer.active && !NetworkClient.active && infosText.text != "") // offline
-            infosText.text = "";
+        NetworkManager manager;
+        [SerializeField] string mainMenuSceneName = "MainMenu";
+        [SerializeField] TMP_Text infosText;
+
+        [Header("Join game")]
+        [SerializeField] TMP_InputField ipAddress;
+        [SerializeField] Button connectGameButton;
+        [SerializeField] Button joinGameButton;
+
+        void Awake()
+        {
+            manager = GetComponent<NetworkManager>();
+        }
+
+        public void StartHostingGame()
+        {
+            NetworkManager.singleton.StartHost(); // starts both host and client
+        }
+
+        public void StopHostingGame()
+        {
+            NetworkManager.singleton.StopHost(); // stop both host and client
+            ReloadMainMenuScene();
+        }
+
+        public void ConnectGame()
+        {
+            NetworkManager.singleton.networkAddress = ipAddress.text;
+            connectGameButton.interactable = false;
+            infosText.text = $"Connecting to {NetworkManager.singleton.networkAddress}...";
+            NetworkManager.singleton.StartClient();
+        }
+
+        public void JoinGame()
+        {
+            NetworkClient.Ready();
+            if (NetworkClient.localPlayer == null)
+                NetworkClient.AddPlayer();
+        }
+
+        public void LeaveGame()
+        {
+            NetworkManager.singleton.StopClient();
+            ReloadMainMenuScene();
+        }
+
+        void ReloadMainMenuScene()
+        {
+            if (manager)
+                Destroy(manager.transform.gameObject);
+            SceneManager.LoadScene(mainMenuSceneName);
+        }
+
+        void Update()
+        {
+            UpdateStatusLabels();
+
+            if (NetworkClient.isConnected && !NetworkClient.ready && !joinGameButton.IsActive())
+                joinGameButton.enabled = true;
+        }
+
+        void UpdateStatusLabels()
+        {
+            if (NetworkServer.active && NetworkClient.active) // host mode
+                infosText.text = $"<b>Host</b>: running via {Transport.activeTransport}";
+            else if (NetworkClient.isConnected) // client only
+                infosText.text = $"<b>Client</b>: connected to {NetworkManager.singleton.networkAddress} via {Transport.activeTransport}";
+            else if (!NetworkServer.active && !NetworkClient.active && infosText.text != "") // offline
+                infosText.text = "";
+        }
     }
 }
